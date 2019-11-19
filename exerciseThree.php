c28662e723c586598b46808a232a0fbea316948e
Rebecca Stevens <rsbjshan@gmail.com>
2019-11-19 12:31:56 +0000

    Exercise three - created a connection to the mysql server with php and created a database with two tables in phpadmin

---
+++ exerciseThree.php
@@ -0,0 +1,34 @@
+
+
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "first_TestDB";
+
+$connection = mysqli_connect($server, $username, $password, $database);
+
+// if($connection){
+//     echo "Database connection was successful";
+// }else{
+//     echo "Connection was not successful" . mysqli_error($connection);
+//     die($connection);
+// }
+
+try {
+    $connection = mysqli_connect($server, $username, $password, $database);
+    if($connection){
+        echo "Database connection was successful";
+    }
+} catch(Exception $errormsg){
+    echo $errormsg->getMessage();
+}
+
+
+
+
+
+
+
+?>
